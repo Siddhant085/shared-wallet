2956871b4312b06450d8435f0b5ad09377955e03
Siddhant Gupta <dpsrkp.sid@gmail.com>
2020-08-20 23:00:34 +0530

    code re-organization

--- wallet.sol
+++ wallet.sol
@@ -3,11 +3,11 @@
 pragma solidity ^0.6.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract wallet {
+contract Allowance is Ownable {
     using SafeMath for uint;
     
-    address payable public owner;
     struct user {
         uint allowance;
         uint balance;
@@ -15,40 +15,16 @@ contract wallet {
     }
     mapping(address => user) record;
     
-    modifier onlyOwner {
-        require(msg.sender == owner);
+    modifier allowWithdraw(uint _amount) {
+        require(owner() == msg.sender || record[msg.sender].balance >= _amount, "Operation not allowed");
         _;
     }
-    
-    constructor() public {
-        owner = msg.sender;
-    }
-    
-    // Try defining a separate function for owner and check if that is more efficient.
-    function getBalance() public view returns(uint) {
-        if (msg.sender == owner) {
-            return address(this).balance;
-        }
-        return record[msg.sender].balance;
-    }
-    
-    function withdrawOwner(uint _amount) public onlyOwner {
-        require(_amount < address(this).balance);
-        owner.transfer(_amount);
-    }
-    
-    function withdrawUser(uint _amount) public {
-        _amount = _amount * 1 ether;
-        require(_amount < record[msg.sender].balance);
-        record[msg.sender].balance = record[msg.sender].balance.sub(_amount);
-        msg.sender.transfer(_amount);
-        
-    }
+
     
     function setAllowance(address _user, uint _amount) public onlyOwner {
         user memory u;
-        u.allowance = _amount * 1 ether;
-        u.balance = _amount * 1 ether;
+        u.allowance = _amount;
+        u.balance = _amount;
         u.active = true;
         record[_user] = u;
     }
@@ -61,7 +37,25 @@ contract wallet {
         return record[_user].allowance;
     }
     
+    // Try defining a separate function for owner and check if that is more efficient.
+    function getBalance() public view returns(uint) {
+        if (msg.sender == owner()) {
+            return address(this).balance;
+        }
+        return record[msg.sender].balance;
+    }
+}
+
+contract wallet is Allowance {
+
+    function withdrawMoney(address payable _to, uint _amount) public allowWithdraw(_amount) {
+        require(_amount <= address(this).balance);
+        record[msg.sender].balance = record[msg.sender].balance.sub(_amount);
+        _to.transfer(_amount);
+        
+    }
     
+
     // Accept whatever funds are sent.
     receive() payable external {}
     
